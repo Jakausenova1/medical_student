b827390dc482cdf21f0e1b91cc8be6f6a4cdfefb
Jaka Usenova <97898976+Jakausenova1@users.noreply.github.com>
2023-03-03 18:30:01 +0600

    chat

--- lib/feature/chat_page/one_chat_page.dart
+++ lib/feature/chat_page/one_chat_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:odnajdyvkgma/core/theme/app_colors.dart';
 
 import '../../core/ui/widgets/chat_header.dart';
+import '../../core/ui/widgets/chat_sample.dart';
+import '../../core/ui/widgets/new_message_widget.dart';
 
 class ChatPage extends StatelessWidget {
   ChatPage({super.key});
@@ -9,10 +12,37 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: ProfileHeaderWidget(
-          name: 'Ajj',
+        child: Container(
+          color: AppColors.chatBackground,
+          child: Column(
+            children: [
+              const ProfileHeaderWidget(
+                name: 'abai tekimbaev',
+              ),
+              Expanded(
+                child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: const BoxDecoration(
+                      color: AppColors.chatBackground,
+                    ),
+                    child: ListView(
+                      children: const [
+                        ChapSample(),
+                        ChapSample(),
+                        ChapSample(),
+                        ChapSample(),
+                        ChapSample(),
+                      ],
+                    )),
+              ),
+              const SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: NewMessageWidget(idUser: ''))
+            ],
+          ),
         ),
       ),
     );
